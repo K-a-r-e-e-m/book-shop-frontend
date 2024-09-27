504dca0401358a0431b4187a5add7e31c9b5fd9c
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-27 21:12:14 +0300

    update new config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,21 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    port: 8080,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  // Catch-all route to serve index.html for unknown paths
+  esbuild: {
+    target: 'esnext',
+  },
+  base: '/',
 });
